528ac06e14d7b5774da701fc158f46db00b225b2
Ziyi Chen <zc2913@nyu.edu>
2023-09-24 20:51:49 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,16 +8,20 @@ function setup() {
 function draw() {
     background("yellow");
     fill("black");
-    for(let xpos = 0; xpos <= width; xpos+= eDiam) {
+    for(let xpos = 0; xpos <= width; xpos += eDiam) {
         for(let ypos = 0; ypos <= height; ypos+= eDiam) {
-            ellipse(xpos, ypos, 5, 5);
+            ellipse(xpos, ypos, 3, 3);
+            ellipse(xpos + eDiam/2, ypos, 10, 10);
     }
 
-    for(let xpos = 15; xpos <= width; xpos+= eDiam) {
-      for(let ypos = 0; ypos <= height; ypos+= eDiam) {
-          ellipse(xpos, ypos, 15, 15);
+    for(let xpos = eDiam/2 ; xpos <= width; xpos+= eDiam) {
+      for(let ypos = eDiam/2 ; ypos <= height; ypos+= eDiam) {
+          ellipse(xpos, ypos, 3, 3);
+          ellipse(xpos + eDiam/2 , ypos, 10, 10);
       }
-    }
+  
+      }
+    
   }
 }
 
